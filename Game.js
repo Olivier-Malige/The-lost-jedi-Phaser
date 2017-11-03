98c416e1413f6feaa89ec98b3eb94db990ed832e
Arkanium <omalige@gmail.com>
2017-11-04 00:20:25 +0100

    Ajout de tout les sprite avec leur laser et physiques arcades pour le moment. ajout du sprite d'explosion

--- Game.js
+++ Game.js
@@ -28,34 +28,57 @@ BasicGame.Game = function (game) {
 BasicGame.Game.prototype = {
     preload : function (){
       this.load.image('gLaser','Assets/gLaser.png');
+      this.load.image('xWing','Assets/xWing.png');
+      this.load.image('rLaser','Assets/rLaser.png');
       this.load.spritesheet('tie','Assets/tie-Sheet.png',8,8);
+      this.load.spritesheet('explosion','Assets/explosion-Sheet.png',8,8);
     },
 
     create: function () {
-        //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+        //Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+
+        //Add Sprites
         this.gLaser = this.add.sprite(512, 400, 'gLaser');
-
+        this.rLaser = this.add.sprite(512,680, 'rLaser');
+        this.xWing = this.add.sprite(512,700 , 'xWing');
+        this.explosion = this.add.sprite(100,100,'explosion')
         this.tie = this.add.sprite(512,380,'tie');
-        this.tie.animations.add('idle',[0,1,2,3],10,true);
 
+        //animates
+        this.tie.animations.add('idle',[0,1,2,3],10,true);
+        this.explosion.animations.add('start',[0,1,2,3,4,5,6,7,8,9],50,false);
 
         //Scale sprites
         this.tie.scale.set(4);
         this.gLaser.scale.set(4);
+        this.xWing.scale.set(4);
+        this.explosion.scale.set(4);
+        this.rLaser.scale.set(4);
 
         //No blur
         this.tie.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
         this.gLaser.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
+        this.rLaser.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
+        this.xWing.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
+        this.explosion.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
 
         //set sprites anchor
         this.tie.anchor.setTo(0.5,0.5);
         this.gLaser.anchor.setTo(0.5,0.5);
+        this.rLaser.anchor.setTo(0.5,0.5);
+        this.explosion.anchor.setTo(0.5,0.5);
+        this.xWing.anchor.setTo(0.5,0.5);
 
         //sprites physics
         this.physics.enable(this.gLaser,Phaser.Physics.ARCADE);
+        this.physics.enable(this.rLaser,Phaser.Physics.ARCADE);
+        this.physics.enable(this.tie,Phaser.Physics.ARCADE);
+        this.physics.enable(this.xWing,Phaser.Physics.ARCADE);
         this.gLaser.body.velocity.y = 600;
+        this.rLaser.body.velocity.y = -600;
 
         this.tie.play('idle');
+        this.explosion.play('start');
     },
 
     update: function () {
